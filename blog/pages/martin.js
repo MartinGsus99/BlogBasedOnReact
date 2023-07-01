0b6cb01506ec48f49374dd183261d054a5747228
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-07-01 23:31:24 +0800

    初始化提交

---
+++ blog/pages/martin.js
@@ -0,0 +1,11 @@
+function Martin(){
+    return (
+        <div>
+        <h1>Martin</h1>
+        <p>My name is Martin</p>
+        <button>Click me</button>
+        </div>
+    )
+}
+
+export default Martin
